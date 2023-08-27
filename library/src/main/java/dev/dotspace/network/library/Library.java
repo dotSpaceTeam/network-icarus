0382475795621a713b15801a9555a8a8fe780ee4
Day-Hawk <daniel.riethmueller@gmx.net>
2023-08-27 11:16:46 +0200

    dev: Added getter to service for library

--- library/src/main/java/dev/dotspace/network/library/Library.java
+++ library/src/main/java/dev/dotspace/network/library/Library.java
@@ -1,5 +1,6 @@
 package dev.dotspace.network.library;
 
+import dev.dotspace.common.response.ResponseService;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 import org.jetbrains.annotations.NotNull;
@@ -12,4 +13,13 @@ public final class Library {
   @Getter
   @Accessors(fluent=true)
   private final static @NotNull LibraryModule module = new LibraryModule();
+
+  /**
+   * Get {@link ResponseService} instance of the library module.
+   *
+   * @return same instance as {@link LibraryModule#responseService()}.
+   */
+  public static @NotNull ResponseService responseService() {
+    return module.responseService();
+  }
 }
--- library/src/main/java/dev/dotspace/network/library/LibraryModule.java
+++ library/src/main/java/dev/dotspace/network/library/LibraryModule.java
@@ -2,7 +2,10 @@ package dev.dotspace.network.library;
 
 import com.google.inject.AbstractModule;
 import dev.dotspace.common.response.ResponseService;
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 import lombok.extern.log4j.Log4j2;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,10 +16,13 @@ import java.util.concurrent.Executors;
  * Singleton library module
  */
 @Log4j2
+@Accessors(fluent=true)
 public final class LibraryModule extends AbstractModule {
+
   /**
    * Service for {@link dev.dotspace.common.response.ResponseService}.
    */
+  @Getter(AccessLevel.PROTECTED)
   private final @NotNull ResponseService responseService;
 
   /**
