7e1967a6e2f850bb6592f7f83a1421b68f8e88f6
Day-Hawk <daniel.riethmueller@gmx.net>
2023-10-14 11:59:21 +0200

    dev: Declared fanout name

--- rabbitmq/src/main/java/dev/dotspace/network/te/test.java
+++ rabbitmq/src/main/java/dev/dotspace/network/te/test.java
@@ -3,15 +3,11 @@ package dev.dotspace.network.te;
 import dev.dotspace.network.library.profile.ImmutableProfile;
 import dev.dotspace.network.library.profile.ProfileType;
 import dev.dotspace.network.rabbitmq.RabbitClient;
-import dev.dotspace.network.rabbitmq.exception.RabbitClientAbsentException;
-import dev.dotspace.network.rabbitmq.exception.RabbitIOException;
-
-import java.nio.charset.StandardCharsets;
 
 
 public class test {
 
-  public static void main(String[] args) throws RabbitClientAbsentException, RabbitIOException {
+  public static void main(String[] args) throws Exception {
 
 
     // final IRabbitClient rabbitClient = new RabbitClient("user", "password", "icarus", "localhost", 5672);
@@ -21,12 +17,11 @@ public class test {
 
     RabbitClient.connect("amqp://user:password@localhost:5672/icarus");
 
+    Thread.sleep(5000L);
+
     RabbitClient
         .client()
-        .newSubscriber("icarus-broadcast", "")
-        .subscribe(bytes -> {
-          System.out.println("Subcriber content : "+new String(String.valueOf(bytes)));
-        })
+        .newSubscriber(RabbitClient.fanoutExchange(), "")
         .subscribe(ImmutableProfile.class, payload -> {
           System.out.println("Headers: "+payload.headers());
           System.out.println("Paylaod: "+payload.payload());
@@ -35,22 +30,7 @@ public class test {
     RabbitClient
         .client()
         .newPublisher()
-        .publish("icarus-broadcast", "icarus-update-2", "Test".getBytes(StandardCharsets.UTF_8), null);
-
-    RabbitClient
-        .client()
-        .newPublisher()
-        .publish("icarus-broadcast", "icarus-update-2", ImmutableProfile.class,
+        .publish(RabbitClient.fanoutExchange(), "", ImmutableProfile.class,
             ImmutableProfile.of(new ImmutableProfile("Test", "Tester", ProfileType.JAVA)), null);
   }
-
-  public static record Test(String value) {
-
-
-    public void print() {
-      System.out.println("hallu :D");
-    }
-
-  }
-
 }
